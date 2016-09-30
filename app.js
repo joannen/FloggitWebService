3d9f026d571a29864122434ee0f7d2886ede9475
Anna Andersson <anna@snigelfart.se>
2016-09-30 11:48:01 +0200

    started on a rest API and added morgan dependencie

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var app = express();
+var routes = require('./routes');
 var http = require('http').Server(app);
 // var io = require('socket-io');
+var jsonParser = require('body-parser').json;
 
+app.use(jsonParser());
+var logger = require('morgan');
 
-app.get('/', function(req, res) {
-	res.status(200).send('hallå världen!');
-});
+app.use(logger('dev'));
+app.use('/postits', routes);
 
+// app.get('/', function(req, res) {
+//     res.status(200).send('hallå världen!');
+// });
 //io.on('connection', function() {
 //	return 'not implemented';
 //});
 
 http.listen(8080, function() {
-	console.log('service started on port 8080, url: http://localhost:8080');
+    console.log('service started on port 8080, url: http://localhost:8080');
 });
---
+++ routes.js
@@ -0,0 +1,60 @@
+'use strict'
+
+var express = require('express');
+var router = express.Router();
+
+//GET /postits
+//Route for returning all postits
+router.get('/', function(req, res) {
+    res.json({
+        response: 'You sent me a GET request'
+    });
+});
+
+//POST /postits
+//Route fore create postits
+router.post('/', function(req, res) {
+    res.json({
+        response: 'You sent me a POST request',
+        body: req.body
+    });
+});
+
+//GET /postits/:id
+//Route for returning specific postit
+router.get('/:id', function(req, res) {
+    res.json({
+        response: 'You sent me a GET request for ID ' + req.params.id
+    });
+});
+
+//POST /postits/:id/info
+//Route fore create postits
+router.post('/:id/info', function(req, res) {
+    res.json({
+        response: 'You sent me a POST request to /info',
+        postitId: req.params.id,
+        body: req.body
+    });
+});
+
+//PUT /postits/:id/info
+//Edit a specific postit
+router.put('/:id/info', function(req, res) {
+    res.json({
+        response: 'You sent me a PUT request to /info',
+        postitId: req.params.id,
+        body: req.body
+    });
+});
+
+//DELETE /postits/:id/info
+//DELETE a specific postit
+router.delete('/:id/info', function(req, res) {
+    res.json({
+        response: 'You sent me a DELETE request to /info',
+        postitId: req.params.id,
+    });
+});
+
+module.exports = router;
