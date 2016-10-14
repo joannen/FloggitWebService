eae6bed002a5c026b505e7dadbf55d51afe8b119
Izumi Suzuki <lindabjalla@gmail.com>
2016-10-14 21:16:11 +0200

    add extra information (notes) to post-it

--- controllers/models/postit.js
+++ controllers/models/postit.js
@@ -2,56 +2,57 @@ var postIts = [];
 
 //använda node-uuid?
 function generateId() {
-    return +(new Date());
+  return +(new Date());
 }
 
-module.exports.getAll = function() {
-    //add validation to handle if list is empty
-    return postIts;
+module.exports.getAll = function () {
+  //add validation to handle if list is empty
+  return postIts;
 };
 
-module.exports.addOrUpdate = function(id, item) {
-    var itemId;
-    var timeCreated;
-    //add validation to see that postit is valid
-    if (!item) {
-        timeCreated = new Date();
-        item = id;
-        item.timeCreated = timeCreated.toLocaleString();
-        itemId = generateId();
-    } else {
-        itemId = id;
+module.exports.addOrUpdate = function (id, item) {
+  var itemId;
+  var timeCreated;
+  //add validation to see that postit is valid
+  if (!item) {
+    timeCreated = new Date();
+    item = id;
+    item.timeCreated = timeCreated.toLocaleString();
+    itemId = generateId();
+  } else {
+    itemId = id;
 
+  }
+  postIts.push({
+    id: itemId,
+    postIt: {
+      title: item.title,
+      text: item.text,
+      timeCreated: item.timeCreated,
+      color: item.color,
+      notes: item.notes
     }
-    postIts.push({
-        id: itemId,
-        postIt: {
-            title: item.title,
-            text: item.text,
-            timeCreated: item.timeCreated,
-            color: item.color
-        }
-    });
-    return itemId;
+  });
+  return itemId;
 };
 
-module.exports.get = function(id) {
-    id = parseInt(id);
-    //add validation if item !exists
-    var itemToReturn = postIts.filter(function(item) {
-        return item.id === id;
-    });
-    return itemToReturn;
+module.exports.get = function (id) {
+  id = parseInt(id);
+  //add validation if item !exists
+  var itemToReturn = postIts.filter(function (item) {
+    return item.id === id;
+  });
+  return itemToReturn;
 };
 
-module.exports.delete = function(id) {
-    id = parseInt(id);
-    //add validation if item !exists
-    var deletedItem = postIts.filter(function(item) {
-        return item.id === id;
-    });
-    postIts.pop({
-      id: id
-    });
-    return deletedItem;
+module.exports.delete = function (id) {
+  id = parseInt(id);
+  //add validation if item !exists
+  var deletedItem = postIts.filter(function (item) {
+    return item.id === id;
+  });
+  postIts.pop({
+    id: id
+  });
+  return deletedItem;
 };
